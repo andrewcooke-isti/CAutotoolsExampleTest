b38b5fc76836c48dc62644143f94b43ae62a3275
andrew cooke <a.cooke@isti.com>
2014-02-18 13:08:37 -0300

    added command line functionality

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include "money.h"
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
 
 /* only main should be in this file, to make all other functions in
    the prograble testable by Check.  in order to test main(), use a
@@ -6,7 +9,28 @@
 */
 
 int
-main (void)
+main (int argc, char** argv)
 {
+  if (argc != 3) {
+    printf("Usage: money cur amount\n");
+    return 1;
+  }
+
+  char *end = argv[2];
+  errno = 0;
+  long amt = strtol(argv[2], &end, 10);
+  if (errno || *end) {
+    printf("Could not parse %s\n", argv[2]);
+    return 2;
+  }
+  if (amt > INT_MAX || amt < INT_MIN) {
+    printf("Amount %ld too large for int\n", amt);
+    return 3;
+  }
+
+  Money *m = money_create((int) amt, argv[1]);
+  printf("%s %d\n", money_currency(m), money_amount(m));
+  money_free(m);
+
   return 0;
 }
