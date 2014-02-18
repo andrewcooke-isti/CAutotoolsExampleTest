8284fcfdda6be06858d4ec7556709539c2a66f6c
andrew cooke <a.cooke@isti.com>
2014-02-18 10:33:08 -0300

    enable xml results in tests

--- tests/check_money.c
+++ tests/check_money.c
@@ -68,6 +68,7 @@ main (void)
   int number_failed;
   Suite *s = money_suite ();
   SRunner *sr = srunner_create (s);
+  srunner_set_xml(sr, "test-results.xml");
   srunner_run_all (sr, CK_NORMAL);
   number_failed = srunner_ntests_failed (sr);
   srunner_free (sr);
